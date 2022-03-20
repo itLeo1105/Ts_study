fb471ec42bb73cf5303075c7505eabbaa9b05dcb
1182648435@qq.com <1182648435@qq.com>
2022-03-20 12:26:32 +0800

    基础类型定义

---
+++ day01/1.类型注解.ts
@@ -0,0 +1,4 @@
+// 类型注解格式  ----   变量名: 类型注解
+let age: number = 18
+let myName: string = " Leo "
+let isTrue: boolean = true
--- day01/ts基础使用.ts
+++ day01/ts基础使用.ts
@@ -2,3 +2,8 @@
 let username:string = 'Leo'
 
 username = 'Leo huang'
+// username = 0
+
+export {} 
+// 非模块环境下 TS 会把声明的变量当成全局变量
+// 到处文件不会影响同一个tsconfig.json 的全局变量名冲突问题
