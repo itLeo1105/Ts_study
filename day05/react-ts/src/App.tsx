431da905b30c80025ba14d1a904d73187bc860a7
Leo <1182648435@qq.com>
2022-03-30 23:00:20 +0800

    useState,useRef的使用及注意事项

--- day05/react-ts/src/App.tsx
+++ day05/react-ts/src/App.tsx
@@ -1,42 +1,26 @@
-import {useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useRef } from "react"
+import "./App.css"
 function App() {
-  // useEffect 是一个hook
-  useEffect(()=>{
-    // 回调函数什么执行
-    window.addEventListener('resize', function(){
-      // 此时只要窗口发生变化就会出发
-      // console.log('窗口变化了');
-    })
-    // useEffect 可以返回内容
-    return ()=>{
-      // 回调函数的意义   settimeout  一直存在  
-      // 执行时机 ----   销毁组件 或者 完成一次更新的时候会执行这个函数
-    }
-  },[])
-  // useEffect()  // 第二个参数不传每次函数更新都会执行
-  // userEffect (, [])  // 只会在渲染时执行第一次
-  // userEffect (, [变量1，.....])  只要依赖的变量发生变化回调函数都会执行
+  const inpuref = useRef<HTMLInputElement>(null)
+  // 此时的ref 类型是 input 类型 所以需指定泛型为 HTMLInputElement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/*获取input dom*/}
+      <input type="text" ref={inpuref} />
+      <div>
+        <button
+          onClick={() => {
+            {
+              /**? 可选链操作符 */
+            }
+            console.log(inpuref.current?.value)
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          打印DOM
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
