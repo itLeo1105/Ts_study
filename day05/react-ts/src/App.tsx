d16cebd1dfe7a17a19f1644d5705abd63be76b6f
Leo <Leo@tencent.com>
2022-04-14 23:44:58 +0800

    todos 增删改

--- day05/react-ts/src/App.tsx
+++ day05/react-ts/src/App.tsx
@@ -1,6 +1,9 @@
 import {useSelector} from  'react-redux'
 import type {TodoList} from './store/reducers/todos'
 import {RootState} from './store/index'
+import {useDispatch} from 'react-redux'
+import { useRef } from 'react'
+import { AddToDo, DelToDo,toggleToDo } from './store/actions/todos'
 function App() {
   // 方式一
   // 没有指定类型  需要指定 泛型类型 <总体的state，获取的state>
@@ -11,15 +14,41 @@ function App() {
   // 可以不指定泛型类型  指定回调参数的类型
   // const todos = useSelector((state:{todos:TodoList}) => state.todos)
   //  使用returnType 导出类型  state 类型不需要手动写
+  const dispatch = useDispatch()
+  const inpRef = useRef<HTMLInputElement>(null)
   const todos = useSelector((state:RootState) => state.todos)
-  
+  const add = ()=>{
+    // 使用ref 获取 input 的值 通过 action 添加到列表
+    if(inpRef.current) {
+      dispatch(AddToDo(inpRef.current.value))
+      inpRef.current.value = '' // 清空 input 的值
+
+      // dispatch({ // 原始分发
+      //   type:'todos/add',
+      //   payload:inpRef.current.value
+      // })
+    }
+  }
+  const del =(id: number)=>{
+    dispatch(DelToDo(id))
+  }
+  const toggle = (id:number)=>{
+    dispatch(toggleToDo(id))
+  }
   return (
     <div>
       <h1>Leo</h1>
       <ul>
         {/*react 中写ts 只要有类型就会有提示*/}
+        <div>
+          <input type="text" ref={inpRef}/> <button onClick={add}>添加任务</button>
+        </div>
+        <hr />
         {todos.map(item =>{
-          return <li key={item.id}>{item.text}</li>
+          return <li key={item.id}>
+            <span onClick={()=>toggle(item.id)} style={{display:'inline-block',width:'300px',color:item.done?'green':'red'}}>{item.text}</span>
+            <button onClick={()=>del(item.id)}>删除</button>
+          </li>
         })}
       </ul>
     </div>
